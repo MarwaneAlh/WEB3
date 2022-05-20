10fa3045d012380087bb3485fd7fc5c105929253
Al haddaoui Marwane <100091401+MarwaneAlh@users.noreply.github.com>
2022-05-20 16:22:31 +0200

    exercice 1.3
    Exercice sur manipulation d'objet est affichage voir code dans classe App.js

--- web3/Exercice/Part1/courseinfo/src/App.js
+++ web3/Exercice/Part1/courseinfo/src/App.js
@@ -4,55 +4,48 @@ function Header({ course }) {
  return <h1>{course}</h1>;
 }
 
-function Part({ part, number_exercice }) {
- return (
- <p> Part:  {part} Number of exercice : {number_exercice} </p>
-)
+function Content({part}){
+  return <div id ="part">Part name : {part.name} , Exercice : {part.exercises} </div>;
+
+
 }
 
-function Content({ parts }) {
- return (
- <>
- {parts.map(({ name, exercises }) => {
- return <
-   Part part={name} number_exercice={exercises} />;
- })}
- </>
- )
-}
-
-function Total({ total }) {
- return <p>Number total of exercises :  {total}</p>;
-}
 
 function App() {
- const course = {
- name: 'Half Stack application development',
- parts: [
- {
- name: 'Fundamentals of React',
- exercises: 10,
- },
- {
- name: 'Using props to pass data',
- exercises: 7,
- },
- {
- name: 'State of a component',
- exercises: 14,
- },
- ],
- };
 
- const total = course.parts.reduce((prev, curr) => prev + curr.exercises, 0);
+ const course = 'Half Stack application development'
+ const part1 = {
+   name: 'Fundamentals of React',
+   exercises: 10
+ }
+ const part2 = {
+   name: 'Using props to pass data',
+   exercises: 7
+ }
+ const part3 = {
+   name: 'State of a component',
+   exercises: 14
+ }
+
+
+ /*const total = course.parts.reduce((prev, curr) => prev + curr.exercises, 0);
+  <Total total={total} />*/
 
  return (
+  
  <>
- <Header course={course.name} />
- <Content parts={course.parts} />
- <Total total={total} />
+ <Header course={course} />
+ <Content part={part1}/>
+ <Content part={part2}/>
+ <Content part={part3}/>
+
+ 
+
  </>
+
  );
+  
+  
 }
 
 export default App;
