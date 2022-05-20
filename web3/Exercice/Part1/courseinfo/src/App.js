1725dcad15ecceb43981892e0d0ea3700fd5bfef
Al haddaoui Marwane <100091401+MarwaneAlh@users.noreply.github.com>
2022-05-20 16:49:38 +0200

    exercice 1.4
    Voir code page app.Js manipulation d'objet et affichage dans fichier html

--- web3/Exercice/Part1/courseinfo/src/App.js
+++ web3/Exercice/Part1/courseinfo/src/App.js
@@ -5,7 +5,11 @@ function Header({ course }) {
 }
 
 function Content({part}){
-  return <div id ="part">Part name : {part.name} , Exercice : {part.exercises} </div>;
+  var names = part.map(function (part) {
+    return part.name + ' ' + part.exercises+' ';
+  });
+  
+  return <div>  {names} </div>;
 
 
 }
@@ -14,18 +18,20 @@ function Content({part}){
 function App() {
 
  const course = 'Half Stack application development'
- const part1 = {
-   name: 'Fundamentals of React',
-   exercises: 10
- }
- const part2 = {
-   name: 'Using props to pass data',
-   exercises: 7
- }
- const part3 = {
-   name: 'State of a component',
-   exercises: 14
- }
+ const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
 
 
  /*const total = course.parts.reduce((prev, curr) => prev + curr.exercises, 0);
@@ -35,9 +41,8 @@ function App() {
   
  <>
  <Header course={course} />
- <Content part={part1}/>
- <Content part={part2}/>
- <Content part={part3}/>
+ <Content part={parts}/>
+
 
  
 
